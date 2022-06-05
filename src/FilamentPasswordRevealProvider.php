7824eb48c75556e5aa54adf84112784b9d34378a
Lukas Frey <mail@lukasfrey.cz>
2022-06-05 11:52:26 +0200

    feat: add configurable copy to clipboard button, made icons configurable using blade icons
    Signed-off-by: Lukas Frey <mail@lukasfrey.cz>

--- src/FilamentPasswordRevealProvider.php
+++ src/FilamentPasswordRevealProvider.php
@@ -9,6 +9,10 @@ class FilamentPasswordRevealProvider extends PluginServiceProvider
 {
     public static string $name = 'filament-password-reveal';
 
+    protected array $styles = [
+        'filament-password-reveal-styles' => __DIR__ . '/../dist/style.css',
+    ];
+
     public function configurePackage(Package $package): void
     {
         $package->name(static::$name)->hasViews();
--- src/Password.php
+++ src/Password.php
@@ -7,4 +7,54 @@ use Filament\Forms\Components\TextInput;
 class Password extends TextInput
 {
     protected string $view = 'filament-password-reveal::password';
+
+    protected string $showIcon = 'heroicon-o-eye';
+    protected string $hideIcon = 'heroicon-o-eye-off';
+    protected string $copyIcon = 'heroicon-o-clipboard';
+
+    protected bool $copyable = false;
+
+    public function copyable($value = true): static
+    {
+        $this->copyable = $value;
+
+        return $this;
+    }
+
+    public function showIcon(string $icon): static
+    {
+        $this->showIcon = $icon;
+
+        return $this;
+    }
+
+    public function hideIcon(string $icon): static
+    {
+        $this->hideIcon = $icon;
+
+        return $this;
+    }
+
+    public function copyIcon(string $icon): static
+    {
+        $this->copyIcon = $icon;
+
+        return $this;
+    }
+
+    public function getShowIcon(): string {
+        return $this->showIcon;
+    }
+
+    public function getHideIcon(): string {
+        return $this->hideIcon;
+    }
+
+    public function getCopyIcon(): string {
+        return $this->copyIcon;
+    }
+
+    public function isCopyable(): bool {
+        return $this->copyable;
+    }
 }
